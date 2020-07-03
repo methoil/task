5c39e4e6e438bdfa2a97fc5f5b9c23f1ce1e714e
methoil <mateev.br@gmail.com>
2020-07-03 00:00:33 -0400

    feat - adding mongoose entries with validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,22 +1,58 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+// const me = new Task({
+//   description: 'Progress in Node Class',
+//   completed: true,
+// });
+
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(val) {
+      if (!validator.isEmail(val)) {
+        throw new Error('email is invalid');
+      }
+    },
   },
   class: {
-    type: Number,
+    type: String,
+    trim: true,
+    default: 'NEET',
+    validate(val) {
+      if (!val || val.length < 3) {
+        throw new Error('Class must be at least 3 characters');
+      }
+    },
   },
 });
 
 const me = new User({
-  name: 'Boris',
-  class: 'Fullstack Engineer',
+  name: 'Pablo',
+  // class: 'Fullstack Engineer',
+  email: 'bloomingdale@crup.io',
 });
 
 me.save()
